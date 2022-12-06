0b17d51e1bd099845c8d10c125d53308c5dcfdf2
AlgimantasKirdeikis <kirdeikisalgimantas@gmail.com>
2022-12-06 12:56:07 +0200

    change delete script

--- delete.py
+++ delete.py
@@ -1,11 +1,16 @@
 import os
 import time
 
+
 DIRECTORY = 'C:/Users/DELL/Desktop/delete_testas'
 SEVEN_DAYS = 60*60*24*7
 
-for filename in os.scandir(DIRECTORY):
-    if (time.time() - os.path.getctime(filename)) > SEVEN_DAYS:
-        # print(filename.name)
-        os.remove(DIRECTORY+"/"+filename.name)
+def delete():
+    for filename in os.scandir(DIRECTORY):
+        if SEVEN_DAYS > (time.time() - os.path.getctime(filename)):
+            print(DIRECTORY+"/"+filename.name)
+            os.chmod(DIRECTORY+"/"+filename.name, 0o777)
+            os.remove(DIRECTORY+"/"+filename.name)
         
+if __name__ == "__main__":
+    delete()
